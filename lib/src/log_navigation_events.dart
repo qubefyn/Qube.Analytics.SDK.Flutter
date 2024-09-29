e40e8e889d17ad6913a3f9a8d5659862dbb077a3
otabekqubefyn <otabek@qubefyn.com>
2024-09-29 13:40:50 +0500

    Adding DeviceID for Log Navigation Events Function

--- lib/src/log_navigation_events.dart
+++ lib/src/log_navigation_events.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 
 class ScreenNavigationObserver extends NavigatorObserver {
@@ -13,7 +15,7 @@ class ScreenNavigationObserver extends NavigatorObserver {
     var nowTime = DateTime.now();
     if (prevScrName != null) {
       var prevScrPath = scrNamePathMap[prevScrName];
-      _logNavigation(
+      _logNavigationAsync(
         prevScrName!,
         prevScrPath!,
         prevInTime!,
@@ -32,7 +34,7 @@ class ScreenNavigationObserver extends NavigatorObserver {
     super.didPop(route, previousRoute);
     var nowTime = DateTime.now();
     var prevScrPath = scrNamePathMap[prevScrName];
-    _logNavigation(
+    _logNavigationAsync(
       prevScrName!,
       prevScrPath!,
       prevInTime!,
@@ -48,7 +50,7 @@ class ScreenNavigationObserver extends NavigatorObserver {
     super.didReplace(newRoute: newRoute, oldRoute: oldRoute);
     var nowTime = DateTime.now();
     var prevScrPath = scrNamePathMap[prevScrName];
-    _logNavigation(
+    _logNavigationAsync(
       prevScrName!,
       prevScrPath!,
       prevInTime!,
@@ -62,14 +64,37 @@ class ScreenNavigationObserver extends NavigatorObserver {
     scrNamePathMap[prevScrName!] = prevScrName!;
   }
 
-  void _logNavigation(
-      String pageTitle, String pagePath, DateTime inTime, DateTime outTime) {
+  Future<String?> getUniqueId() async {
+    final DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+    if (Platform.isAndroid) {
+      AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+      return androidInfo.id; // This is the Android device ID.
+    } else if (Platform.isIOS) {
+      IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
+      return iosInfo.identifierForVendor; // This is the iOS device identifier.
+    }
+    return null;
+  }
+
+  // New method that handles device ID asynchronously
+  Future<void> _logNavigationAsync(String pageTitle, String pagePath,
+      DateTime inTime, DateTime outTime) async {
+    String? deviceID =
+        await getUniqueId(); // Fetch the device ID asynchronously
+    if (deviceID != null) {
+      _logNavigation(pageTitle, pagePath, inTime, outTime, deviceID);
+    }
+  }
+
+  void _logNavigation(String pageTitle, String pagePath, DateTime inTime,
+      DateTime outTime, String deviceID) {
     final timestamp = DateTime.now();
     final log = NavigationLog(
       pageTitle: pageTitle,
       pagePath: pagePath,
       inTime: inTime,
       outTime: outTime,
+      deviceID: deviceID,
     );
 
     navigationLogs.add(log);
@@ -82,16 +107,137 @@ class NavigationLog {
   final String pagePath;
   final DateTime inTime;
   final DateTime outTime;
+  final String deviceID;
 
   NavigationLog({
     required this.pageTitle,
     required this.pagePath,
     required this.inTime,
     required this.outTime,
+    required this.deviceID,
   });
 
   @override
   String toString() {
-    return 'Page Title: $pageTitle, Page Path: $pagePath, In Time: $inTime, Out Time: $outTime';
+    return 'Page Title: $pageTitle, Page Path: $pagePath, In Time: $inTime, Out Time: $outTime, Device ID: $deviceID';
   }
 }
+
+
+// import 'dart:io';
+
+// import 'package:device_info_plus/device_info_plus.dart';
+// import 'package:flutter/material.dart';
+
+// class ScreenNavigationObserver extends NavigatorObserver {
+//   final List<NavigationLog> navigationLogs = [];
+//   final List<String> scrNameStack = [];
+//   String? prevScrName;
+//   DateTime? prevInTime;
+//   final Map<String, String> scrNamePathMap = {};
+
+//   @override
+//   void didPush(Route route, Route? previousRoute) {
+//     super.didPush(route, previousRoute);
+//     var nowTime = DateTime.now();
+//     if (prevScrName != null) {
+//       var prevScrPath = scrNamePathMap[prevScrName];
+//       _logNavigation(
+//         prevScrName!,
+//         prevScrPath!,
+//         prevInTime!,
+//         nowTime,
+//       );
+//     }
+//     scrNameStack.add(route.settings.name!);
+//     scrNamePathMap[route.settings.name!] =
+//         scrNameStack.join("/").replaceAll("//", "/");
+//     prevScrName = route.settings.name!;
+//     prevInTime = nowTime;
+//   }
+
+//   @override
+//   void didPop(Route route, Route? previousRoute) {
+//     super.didPop(route, previousRoute);
+//     var nowTime = DateTime.now();
+//     var prevScrPath = scrNamePathMap[prevScrName];
+//     _logNavigation(
+//       prevScrName!,
+//       prevScrPath!,
+//       prevInTime!,
+//       nowTime,
+      
+//     );
+//     prevScrName = previousRoute!.settings.name!;
+//     prevInTime = nowTime;
+//     scrNameStack.removeLast();
+//   }
+
+//   @override
+//   void didReplace({Route? newRoute, Route? oldRoute}) {
+//     super.didReplace(newRoute: newRoute, oldRoute: oldRoute);
+//     var nowTime = DateTime.now();
+//     var prevScrPath = scrNamePathMap[prevScrName];
+//     _logNavigation(
+//       prevScrName!,
+//       prevScrPath!,
+//       prevInTime!,
+//       nowTime,
+
+//     );
+//     prevScrName = newRoute!.settings.name!;
+//     prevInTime = nowTime;
+//     scrNameStack.clear();
+//     scrNameStack.add(prevScrName!);
+//     scrNamePathMap.clear();
+//     scrNamePathMap[prevScrName!] = prevScrName!;
+//   }
+
+//   Future<String?> getUniqueId() async {
+//     final DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+//     if (Platform.isAndroid) {
+//       AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+//       return androidInfo.id; // This is the Android device ID.
+//     } else if (Platform.isIOS) {
+//       IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
+//       return iosInfo.identifierForVendor; // This is the iOS device identifier.
+//     }
+//     return null;
+//   }
+
+//   void _logNavigation(String pageTitle, String pagePath, DateTime inTime,
+//       DateTime outTime, String deviceID) {
+//     final timestamp = DateTime.now();
+//     final log = NavigationLog(
+//       pageTitle: pageTitle,
+//       pagePath: pagePath,
+//       inTime: inTime,
+//       outTime: outTime,
+//       deviceID: deviceID,
+//     );
+
+//     navigationLogs.add(log);
+//     print(log); // For debugging purposes
+//   }
+// }
+
+// class NavigationLog {
+//   final String pageTitle;
+//   final String pagePath;
+//   final DateTime inTime;
+//   final DateTime outTime;
+//   final String deviceID;
+
+//   NavigationLog({
+//     required this.pageTitle,
+//     required this.pagePath,
+//     required this.inTime,
+//     required this.outTime,
+//     required this.deviceID,
+//   });
+
+//   @override
+//   String toString() {
+//     return 'Page Title: $pageTitle, Page Path: $pagePath, In Time: $inTime, Out Time: $outTime, Device ID: $deviceID';
+//   }
+// }
